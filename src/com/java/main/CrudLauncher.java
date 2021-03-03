e4d3c6a2e571cafd5434cc787d6a07eedd28d783
JohnnyTB <jtrejosb@live.com>
2021-03-03 11:40:37 -0500

    Application Launcher

---
+++ src/com/java/main/CrudLauncher.java
@@ -0,0 +1,30 @@
+/**
+* Developed by Johnny Trejos Barrios.
+* Technology: Java.
+* Version: Java Development Kit 11.0.4 LTS, Standard Edition.
+* Development Environment: VIM 8.0
+* Date: 26/02/2021, Time: 14:20:40.
+*
+* Additional Info.
+*
+* Source Code Target Or Details:
+*
+*     []
+*
+* Licenses: GNU GPL v3.0, Eclipse Public License 1.0, personal for non-commercial purposes.
+* Developer Contact: jtrejosb@live.com
+* GitHub.com/jtrejosb
+*/
+package com.java.main;
+
+import com.java.controller.CrudController;
+
+public class CrudLauncher {
+  private void testCall() {
+    //Test callings goes here
+  }
+  public static void main(String[] args) {
+    //new CrudController();
+    new CrudLauncher().testCall();
+  }
+}
