111929b3ff10bf16cf08b1a7e53879aa7a63d233
jtrejosb <jtrejosb@live.com>
2021-03-23 18:55:25 -0500

    Added new class not implemented yet

---
+++ src/io/jtrejosb/view/core/FieldMode.java
@@ -0,0 +1,8 @@
+package io.jtrejosb.view;
+
+public interface FieldMode {
+  public static final int CREATION_MODE=1;
+  public static final int READ_MODE=2;
+  public static final int UPDATE_MODE=3;
+  public static final int DELETION_MODE=4;
+}
---
+++ src/io/jtrejosb/view/core/FieldsPane.java
@@ -0,0 +1,160 @@
+package io.jtrejosb.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class FieldsPane extends javax.swing.JPanel implements FieldMode {
+  private Field fCode;
+  private Field fName;
+  private Field fAge;
+  private Field fPhone;
+  private Field fEmail;
+  private Field fJob;
+  private JTextField fFind;
+  private JButton B;
+  public FieldsPane() {
+    fCode=new Field("Code");
+    fName=new Field("Full Name");
+    fAge=new Field("Age");
+    fPhone=new Field("Phone Number");
+    fEmail=new Field("Email Address");
+    fJob=new Field("Occupation");
+    fFind=new JTextField() {
+      @Override
+      public void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.GRAY);
+        if(getText().isEmpty())
+          g.drawString("Client ID",6,18);
+      }
+    };
+    fFind.setPreferredSize(new Dimension(150,25));
+    fFind.setForeground(fCode.getForeground());
+    B=new JButton();
+    B.setEnabled(false);
+
+    add(fCode);
+    add(fName);
+    add(fAge);
+    add(fPhone);
+    add(fEmail);
+    add(fJob);
+    add(fFind);
+    add(B);
+  }
+
+  public void setCode(String data) {
+    fCode.setText(data);
+  }
+
+  public void setName(String data) {
+    fName.setText(data);
+  }
+
+  public void setAge(String data) {
+    fAge.setText(data);
+  }
+
+  public void setPhone(String data) {
+    fPhone.setText(data);
+  }
+
+  public void setJob(String data) {
+    fJob.setText(data);
+  }
+
+  public void setEmail(String data) {
+    fEmail.setText(data);
+  }
+
+  public void setDisplayMode(int mode) {
+    switch(mode) {
+      case CREATION_MODE: mode1();
+        break;
+      case READ_MODE: mode2();
+        break;
+      case UPDATE_MODE: mode3();
+        break;
+      case DELETION_MODE: mode4();
+        break;
+    }
+  }
+
+  public void clearAllFields() {
+    fCode.setText("");
+    fName.setText("");
+    fAge.setText("");
+    fPhone.setText("");
+    fEmail.setText("");
+    fJob.setText("");
+    fFind.selectAll();
+  }
+
+  public Field[] getDataFields() {
+    return new Field[]{fCode,fName,fAge,fPhone,fEmail,fJob};
+  }
+
+  public JTextField getFinderField() {
+    return fFind;
+  }
+
+  public JButton getButton() {
+    return B;
+  }
+
+  private void mode1() {
+    B.setText("Create");
+    remove(fFind);
+  }
+
+  private void mode2() {
+    fCode.setEditable(false);
+    fName.setEditable(false);
+    fAge.setEditable(false);
+    fPhone.setEditable(false);
+    fEmail.setEditable(false);
+    fJob.setEditable(false);
+    B.setVisible(false);
+  }
+
+  private void mode3() {
+    mode2();
+    B.setText("Update");
+    B.setVisible(true);
+  }
+
+  private void mode4() {
+    mode2();
+    B.setText("Delete");
+    B.setVisible(true);
+  }
+}
+
+@SuppressWarnings("serial")
+class Field extends JTextField {
+  private final String ph;
+  public Field(String ph) {
+    this.ph=ph;
+    setEnabled(true);
+    setPreferredSize(new Dimension(150,25));
+    setForeground(Color.DARK_GRAY);
+    setToolTipText("");
+  }
+
+  @Override
+  public void paintComponent(java.awt.Graphics g) {
+    super.paintComponent(g);
+    g.setColor(Color.GRAY);
+    if(getText().isEmpty())
+      g.drawString(ph,6,18);
+  }
+
+  @Override
+  public String getToolTipText(java.awt.event.MouseEvent evt) {
+    return isEditable()?"":"READ ONLY";
+  }
+}
